43febca3068993fd0a29020655dce828e07238ee
ppprka <p.dancko@gmail.com>
2021-10-05 23:15:50 +0300

    v7

--- src/test/java/test/StudentServiceImplTest.java
+++ src/test/java/test/StudentServiceImplTest.java
@@ -1,53 +1,63 @@
 package test;
 
-import org.junit.Assert;
-import org.junit.Before;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.powermock.reflect.Whitebox;
 import ru.danko.testtask.dao.StudentDao;
 import ru.danko.testtask.dao.impl.StudentDaoImpl;
 import ru.danko.testtask.entity.Student;
 import ru.danko.testtask.exception.DaoServiceException;
 import ru.danko.testtask.services.StudentService;
 import ru.danko.testtask.services.impl.StudentServiceImpl;
-import ru.danko.testtask.validator.StudentValidator;
-
 import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.*;
 
-@RunWith(JUnit4.class)
 public class StudentServiceImplTest {
 
-    private static final String TEST_SURNAME = "surname";
+    private static StudentDao studentDao;
+    private static StudentService studentService;
+    public static final String ID = "id";
 
-    @Mock
-    private StudentValidator studentValidator;
+    @BeforeClass
+    public static void setUp() {
+        studentDao = mock(StudentDaoImpl.class);
+        Whitebox.setInternalState(StudentDaoImpl.class, "INSTANCE", studentDao);
+        studentService = new StudentServiceImpl();
+    }
 
-    @Mock
-    private StudentDaoImpl studentDao;
-
-    @InjectMocks
-    private StudentServiceImpl service;
-
-    @Before
-    public void before() {
-        MockitoAnnotations.openMocks(this);
+    @AfterClass
+    public static void tearDown() {
+        studentDao = null;
+        studentService = null;
     }
 
     @Test
-    public void testGetStudentBySurname() throws DaoServiceException {
-        Mockito.when(studentValidator.isNameValid(TEST_SURNAME)).thenReturn(true);
-        Student actual = new Student();
-        actual.setSurname(TEST_SURNAME);
-        Mockito.when(studentDao.findBySurname(TEST_SURNAME)).thenReturn(java.util.Optional.of(actual));
-        Optional<Student> expected = service.findBySurname(TEST_SURNAME);
-        Assert.assertEquals(expected, Optional.of(actual));
+    public void findByIdPositiveTest() {
+        try {
+            Student expected = new Student();
+            when(studentDao.findById(any(Long.class))).thenReturn(Optional.of(new Student()));
+            Optional<Student> actual = studentService.findById("1");
+            assertEquals(actual.get(), expected);
+        } catch (DaoServiceException e) {
+            fail("Incorrect data", e);
+        }
+    }
+
+    @Test
+    public void findByIdNegativeTest() {
+        try {
+            Optional<Student> expected = Optional.of(new Student());
+            when(studentDao.findById(any(Long.class))).thenReturn(Optional.of(new Student()));
+            Optional<Student> actual = studentService.findById(" ");
+            assertNotEquals(actual, expected);
+        } catch (DaoServiceException e) {
+            fail("Incorrect data", e);
+        }
     }
 
 }
